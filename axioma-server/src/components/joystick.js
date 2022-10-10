b3684a64c3e18a027a3e73529b90ef939b4585db
Mario.Alvarez <mario.alvarez@grupomok.com>
2022-10-09 19:43:44 -0500

    primer commit

---
+++ axioma-server/src/components/joystick.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const joystick = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default joystick
---
+++ axioma-server/src/view/homeIn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Joystick } from 'react-joystick-component';
+
+
+const homeIn = () => {
+    return (
+        <div>
+            kkkk
+        </div>
+    )
+}
+
+export default homeIn
